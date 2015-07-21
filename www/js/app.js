ba486e39447a87d97b58b9e82e2abf4b765a9197
Sebastián Osorio <svzosorio@boolinc.co>
2015-07-21 16:51:45 -0500

    Initialize project

---
+++ www/js/app.js
@@ -0,0 +1,36 @@
+(function(){
+
+    angular
+    .module('CAssessment',['ionic'] )
+    .config(config)
+    .run(run);
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+        .state('home', {
+            url: '/',
+            templateUrl: 'templates/home.html'
+        });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/');
+
+    }
+
+    function run($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
+    }
+})();
