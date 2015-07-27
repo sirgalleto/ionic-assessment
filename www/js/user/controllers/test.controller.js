bdafbf0f5c13a9140fccdaa938b121b0429bb2fd
Sebastián Osorio <svzosorio@boolinc.co>
2015-07-27 04:11:39 -0500

    add response test logic

--- www/js/user/controllers/test.controller.js
+++ www/js/user/controllers/test.controller.js
@@ -2,37 +2,27 @@
     'use strict';
 
     angular
-        .module('CAssessment.user')
-        .controller('TestController', TestController);
+    .module('CAssessment.user')
+    .controller('TestController', TestController);
 
     function TestController($scope, $state, tests, $ionicPopup) {
 
-        $scope.test = tests[$state.params.id].name;
+        $scope.test = tests[$state.params.id];
         init();
 
         function init() {
-
-            $ionicPopup.show({
-                template: '<input type="text" ng-model="user">',
-                title: 'Whats your name?',
-                scope: $scope,
-                buttons: [
-                    {
-                        text: '<b>Ok</b>',
-                        type: 'button-balanced',
-                        onTap: function(e){
-                            if(!$scope.user){
-                                e.preventDefault();
-                            }
-                            else{
-                                return $scope.user.name;
-                            }
-                        }
-                    }
-                ]
-
-            }).then(function(res){
-                $scope.user.name = res;
+            $ionicPopup.prompt({
+                title: "What's your name?",
+                inputType: 'text',
+                inputPlaceholder: 'Name',
+                okType: 'button-balanced'
+            }).then(function(res) {
+                if(res){
+                    $scope.user = res;
+                }
+                else{
+                    $state.go('home');
+                }
             });
         }
     }
--- www/js/user/services/tests.constant.js
+++ www/js/user/services/tests.constant.js
@@ -3,7 +3,30 @@
 
     var tests = [
         {
-            name: 'first'
+            name: 'first',
+            questions:[
+                {
+                    title:'Question 1',
+                    answers: {
+                        a: 'This is a answer ',
+                        b: 'This is a second answer',
+                        c: 'This is a third answer',
+                        d: 'This is a finish answer'
+                    },
+                    correct: 'a'
+                },
+
+                {
+                    title:'Question 2',
+                    answers: {
+                        a: 'This is a answer ',
+                        b: 'This is a second answer',
+                        c: 'This is a third answer',
+                        d: 'This is a finish answer'
+                    },
+                    correct: 'a'
+                }
+            ]
         },
         {
             name: 'second'
