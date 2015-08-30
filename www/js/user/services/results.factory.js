07f8b129e4e9b2816b79d06aa69a171bc2e97211
Sebastián Osorio <im@svzosorio.co>
2015-08-30 01:20:42 -0500

    Solve, verify if result exist

--- www/js/user/services/results.factory.js
+++ www/js/user/services/results.factory.js
@@ -8,13 +8,20 @@
     function Results(localStorageService) {
 
         return{
+
             get: function(){
                 return localStorageService.get('results');
             },
             add: function(result){
-                localStorageService.set(
-                    localStorageService.get('results').push(result)
+
+                var results = localStorageService.get(
+                    'results'
                 );
+
+                if(!results) results = [result];
+                else results.push(result);
+
+                localStorageService.set('results', results);
             }
         };
     }
