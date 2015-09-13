62bff2ed2749fb4b3fa68a3fc0e27cf07155e531
Sebastián Osorio <im@svzosorio.co>
2015-09-13 15:28:24 -0500

    show popup results

--- www/js/user/controllers/results.controller.js
+++ www/js/user/controllers/results.controller.js
@@ -5,8 +5,22 @@
     .module('CAssessment.user')
     .controller('ResultsController', ResultsController);
 
-    function ResultsController($scope, Results) {
+    function ResultsController($scope, $ionicPopup, Results) {
 
         $scope.results = Results.get();
+
+        $scope.showResult = function(result){
+
+            $scope.user = result.name;
+            $scope.result = result.result;
+            $scope.total = result.total;
+
+            $ionicPopup.alert({
+                title: $scope.user,
+                templateUrl: 'templates/result.html',
+                scope: $scope,
+                okType: 'button-balanced'
+            });
+        };
     }
 })();
